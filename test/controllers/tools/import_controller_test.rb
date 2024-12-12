e35bf8950daa06a9647dcdb29db28b8acd01bcaa
Versun <me+github7604@versun.org>
2024-12-13 05:40:58 +0800

    remove all test

--- test/controllers/tools/import_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ImportController  < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/jobs/publish_scheduled_articles_job_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PublishScheduledArticlesJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
