d575153cd8bbd93215760e5c90eb72cab1c8d7a9
Versun <me+github7604@versun.org>
2025-01-22 21:02:58 +0800

    feat: enhance database and job queue configuration
    These changes enhance the database configuration by introducing separate databases for primary and queue operations, configure the active job queue adapter, and update the default settings initializer to include necessary URL helpers. Additionally, the queue schema has been updated to reflect the latest database version and structure.

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,9 +1,9 @@
 # config/initializers/default_settings.rb
 Rails.application.config.after_initialize do
+  include Rails.application.routes.url_helpers
   # 使用 Rails.application.executor.wrap 确保在完整的 Rails 环境中执行
   Rails.application.executor.wrap do
     next unless defined?(Setting) # 确保 Setting 模型已加载
-
     # 检查表是否存在
     next unless ActiveRecord::Base.connection.table_exists?("settings")
 
@@ -21,7 +21,7 @@ Rails.application.config.after_initialize do
         begin
           default_url_options = Rails.application.config.action_mailer.default_url_options ||
                                { host: "localhost", port: 3000 }
-          setting.url = Rails.application.routes.url_helpers.root_url(**default_url_options)
+          setting.url = root_url(**default_url_options)
         rescue => e
           # 如果 URL 生成失败，使用默认值
           setting.url = "http://localhost:3000"
