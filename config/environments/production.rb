55eb2ae9e069b649d4017f4ed727d9957667ac06
Versun <me+github7604@versun.org>
2025-01-27 13:37:19 +0800

    feat: configure active storage delivery method in production
    This change adds a delivery method configuration for active storage in the production environment, specifying the use of a proxy, which is essential for handling file uploads correctly in a production setup.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :s3
+  config.active_storage.delivery_method = :proxy
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
