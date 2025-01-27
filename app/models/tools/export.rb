58f822bdba4af8fcee4e7bf19a1ca6703291e1a1
Versun <me+github7604@versun.org>
2025-01-27 14:47:40 +0800

    rubocop -A

--- app/models/tools/export.rb
+++ app/models/tools/export.rb
@@ -11,7 +11,6 @@ module Tools
     end
 
     def generate
-        
     end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,5 +72,4 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
-
 end
