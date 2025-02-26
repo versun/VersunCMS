4302f97227990bfb8d5948c32477ec26a5674cb9
Versun <me+github7604@versun.org>
2025-02-27 07:13:06 +0800

    update

--- app/jobs/listmonk_sender_job.rb
+++ app/jobs/listmonk_sender_job.rb
@@ -8,31 +8,8 @@ class ListmonkSenderJob < ApplicationJob
     article = Article.find(article_id)
     listmonk = Listmonk.first
 
-    return unless listmonk.present? && listmonk.list_id.present?
+    return unless listmonk.present? && listmonk.list_id.present? && listmonk.template_id.present?
 
-    send_to_listmonk(article, listmonk)
-  end
-
-  private
-
-  def send_newsletter(article, listmonk)
-    return false unless listmonk.list_id.present?
-
-    uri = URI("#{url}/api/campaigns")
-    request = Net::HTTP::Post.new(uri)
-    request["X-API-Key"] = listmonk.api_key
-    request["Content-Type"] = "application/json"
-    request.body = {
-      name: article.title,
-      content_type: "richtext",
-      body: article.content,
-      list_ids: [listmonk.list_id]
-    }.to_json
-
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-      http.request(request)
-    end
-
-    response.is_a?(Net::HTTPSuccess)
+    listmonk.send_newsletter(article, site_settings[:title])
   end
 end
