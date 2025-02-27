74306550148327cc093316f4d0e89c4f3ff41604
Versun <me+github7604@versun.org>
2025-02-28 06:21:56 +0800

    test: remove placeholder tests for newsletters controller
    The default controller tests were placeholders and not useful. Removing them to start with a clean slate for proper testing.

--- test/controllers/newsletters_controller_test.rb
+++ test/controllers/newsletters_controller_test.rb
@@ -1,18 +1,18 @@
 require "test_helper"
 
 class NewslettersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get newsletters_index_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get newsletters_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get newsletters_update_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get newsletters_index_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get newsletters_edit_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get newsletters_update_url
+  #   assert_response :success
+  # end
 end
