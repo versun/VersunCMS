c03a66ec100ee8ae816f702356b1de326af06f6d
Versun <me+github7604@versun.org>
2025-03-04 19:32:57 +0800

    feat: enhance analytics dashboard
    This commit enhances the analytics dashboard by adding the following features:
    
    - Limits the number of displayed pages to the top 10 most visited.
    - Sorts referrers by visit count in descending order.
    - Updates the view to display the count and the data with a separator.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,13 +2,22 @@ class AnalyticsController < ApplicationController
   def index
     @total_visits = Ahoy::Visit.count
     events = Ahoy::Event.where(name: "Viewed").where("properties->>'slug' IS NOT NULL")
-    @visits_by_path = {}
-    events.each do |event|
-      @visits_by_path[event.properties] ||= 0
-      @visits_by_path[event.properties] += 1
-    end
 
-    @referrers = Ahoy::Visit.group(:referrer).count.reject { |k, _| k.nil? }
+    # 统计访问量并按数量排序，只取前10篇
+    visits_count = {}
+    events.each do |event|
+      visits_count[event.properties] ||= 0
+      visits_count[event.properties] += 1
+    end
+    @visits_by_path = visits_count.sort_by { |_, count| -count }.first(10).to_h
+
+    # 按引荐来源数量从大到小排序
+    @referrers = Ahoy::Visit.group(:referrer)
+                            .count
+                            .reject { |k, _| k.nil? }
+                            .sort_by { |_, count| -count }
+                            .to_h
+
     @browsers = Ahoy::Visit.group(:browser).count
     @operating_systems = Ahoy::Visit.group(:os).count
     @devices = Ahoy::Visit.group(:device_type).count
