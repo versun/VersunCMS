ce01958679007d29e9f6554a40e99dab23c35a0a
Versun <me+github7604@versun.org>
2025-03-06 01:22:45 +0000

    feat: only get first 20 audits for pages and referrers

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,18 +3,19 @@ class AnalyticsController < ApplicationController
     @total_visits = Ahoy::Visit.count
     events = Ahoy::Event.where(name: "Viewed").where("properties->>'slug' IS NOT NULL")
 
-    # 统计访问量并按数量排序，只取前10篇
+    # 统计访问量并按数量排序，只取前20篇
     visits_count = {}
     events.each do |event|
       visits_count[event.properties] ||= 0
       visits_count[event.properties] += 1
     end
-    @visits_by_path = visits_count.sort_by { |_, count| -count }.first(10).to_h
+    @visits_by_path = visits_count.sort_by { |_, count| -count }.first(20).to_h
 
     @referrers = Ahoy::Visit.group(:referrer)
                             .count
                             .reject { |k, _| k.nil? }
                             .sort_by { |_, count| -count }
+                            .first(20)
                             .to_h
 
     @browsers = Ahoy::Visit.group(:browser)
