92d40a37b67b66d5fff9c7fd60f852e6779a9549
Versun <me+github7604@versun.org>
2025-03-12 16:49:50 +0800

    feat: prevent duplicate scheduled article jobs
    This commit introduces a mechanism to cancel existing scheduled jobs for an article before scheduling a new one. This prevents duplicate jobs from running if the scheduled time is updated.

--- app/jobs/publish_scheduled_articles_job.rb
+++ app/jobs/publish_scheduled_articles_job.rb
@@ -2,6 +2,16 @@ class PublishScheduledArticlesJob < ApplicationJob
   queue_as :urgent
   queue_with_priority 10
 
+  def self.cancel_old_jobs(article_id)
+    Rails.logger.info "Checking for scheduled job for article #{article_id}"
+    ActiveJob::Base.jobs.scheduled.where(job_class_name: "PublishScheduledArticlesJob").each do |job|
+      if job.arguments[0]["arguments"] == [ article_id ]
+        Rails.logger.info "Cancelling old job for article #{article_id}"
+        job.discard
+      end
+    end
+  end
+
   def perform(article_id)
     article = Article.find(article_id)
     Rails.logger.info "Publishing scheduled article #{article_id} at #{Time.current}"
@@ -12,6 +22,7 @@ class PublishScheduledArticlesJob < ApplicationJob
 
   def self.schedule_at(article)
     return unless article.schedule? && article.scheduled_at.present?
+    cancel_old_jobs(article.id)
 
     # The scheduled_at value is already in UTC in the database
     # We only need to convert it to the application time zone for display/job scheduling
@@ -22,7 +33,7 @@ class PublishScheduledArticlesJob < ApplicationJob
     Rails.logger.info "Scheduled article #{article.id} for publication at #{scheduled_time}"
   end
 
-  private
+  # private
 
   # def self.scheduled_job_for(article)
   #   ActiveJob::Base.queue_adapter.enqueued_jobs.find do |job|
