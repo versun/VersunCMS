e505682bc3938aba4cfa63b663aa64687af270b4
Versun <me+github7604@versun.org>
2025-04-15 02:28:49 +0000

    Remove browser version restriction in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   protect_from_forgery with: :exception
 
   helper_method :navbar_items
