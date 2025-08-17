ece67811140a3ae73fc5cfbf7f9c6e7561bca918
Versun <me+github7604@versun.org>
2025-08-17 16:12:56 +0800

    实现导出文章为CSV功能，更新ExportController和Export模型，添加错误处理机制。

--- app/controllers/tools/export_controller.rb
+++ app/controllers/tools/export_controller.rb
@@ -1,9 +1,21 @@
- module Tools
-    class ExportController < ApplicationController
-      def index
-      end
+module Tools
+  class ExportController < ApplicationController
+    def index
+    end
 
-      def create
+    def create
+      export_tool = Tools::Export.new
+      csv_data = export_tool.generate_csv
+      
+      if csv_data
+        send_data csv_data, 
+                  filename: "articles_#{Date.current.strftime('%Y%m%d')}.csv",
+                  type: 'text/csv',
+                  disposition: 'attachment'
+      else
+        flash[:error] = export_tool.error_message || "导出失败"
+        redirect_to tools_export_index_path
       end
     end
- end
+  end
+end
--- app/models/tools/export.rb
+++ app/models/tools/export.rb
@@ -1,6 +1,6 @@
 module Tools
   class Export
-    # TODO：重构为导出文章为csv格式
+    require "csv"
     require "zip"
 
     attr_reader :zip_path, :error_message
@@ -10,7 +10,38 @@ module Tools
       @error_message = nil
     end
 
+    def generate_csv
+      begin
+        articles = Article.all
+        
+        csv_data = CSV.generate(headers: true) do |csv|
+          csv << ['ID', 'Title', 'Slug', 'Description', 'Status', 'Scheduled At', 'Created At', 'Updated At', 'Is Page', 'Page Order']
+          
+          articles.each do |article|
+            csv << [
+              article.id,
+              article.title,
+              article.slug,
+              article.description,
+              article.status,
+              article.scheduled_at,
+              article.created_at,
+              article.updated_at,
+              article.is_page,
+              article.page_order
+            ]
+          end
+        end
+        
+        csv_data
+      rescue => e
+        @error_message = "导出失败: #{e.message}"
+        nil
+      end
+    end
+
     def generate
+      generate_csv
     end
   end
 end
