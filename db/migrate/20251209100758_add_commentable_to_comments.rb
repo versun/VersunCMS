a8d68aa97c21e51e037bb4ec2c2e2b824449cefb
versun <2398708+versun@users.noreply.github.com>
2025-12-09 02:40:47 +0000

    style: auto fix by rubocop

--- db/migrate/20251209100758_add_commentable_to_comments.rb
+++ db/migrate/20251209100758_add_commentable_to_comments.rb
@@ -3,17 +3,17 @@ class AddCommentableToComments < ActiveRecord::Migration[8.1]
     # Add polymorphic association columns
     add_column :comments, :commentable_type, :string
     add_column :comments, :commentable_id, :integer
-    
+
     # Migrate existing data: set commentable to Article for all existing comments
     execute <<-SQL
-      UPDATE comments 
-      SET commentable_type = 'Article', commentable_id = article_id 
+      UPDATE comments#{' '}
+      SET commentable_type = 'Article', commentable_id = article_id#{' '}
       WHERE article_id IS NOT NULL
     SQL
-    
+
     # Add index for polymorphic association
-    add_index :comments, [:commentable_type, :commentable_id]
-    
+    add_index :comments, [ :commentable_type, :commentable_id ]
+
     # Make article_id nullable (we'll keep it for backward compatibility during transition)
     change_column_null :comments, :article_id, true
   end
