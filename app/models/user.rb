a5de5c5213438c0b8ae49d0ae88a23838547252c
Versun <me+github7604@versun.org>
2025-12-20 17:47:16 +0800

    给user添加validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   normalizes :user_name, with: ->(e) { e.strip.downcase }
+
+  validates :user_name, presence: true, uniqueness: true
 end
