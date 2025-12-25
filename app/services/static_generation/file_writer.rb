2332858f048d81925c9c33caf40c6b1f8e771a33
Versun <me+github7604@versun.org>
2025-12-25 11:41:25 +0800

    修复编码问题

--- app/services/static_generation/file_writer.rb
+++ app/services/static_generation/file_writer.rb
@@ -16,7 +16,15 @@ module StaticGeneration
         content = @rewriter.rewrite_rss(content)
       end
 
-      File.write(full_path, content)
+      content = content.to_s
+      unless content.encoding == Encoding::UTF_8
+        begin
+          content = content.encode(Encoding::UTF_8)
+        rescue Encoding::UndefinedConversionError, Encoding::InvalidByteSequenceError
+          content = content.dup.force_encoding(Encoding::UTF_8)
+        end
+      end
+      File.binwrite(full_path, content)
       @event.notify("static_generator.file_written", level: "debug", component: "StaticGenerator", path: relative_path)
     end
   end
