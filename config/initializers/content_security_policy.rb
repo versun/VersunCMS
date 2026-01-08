61ebef3b080ec921b5e319d21fe9403040b70a0c
versun <2398708+versun@users.noreply.github.com>
2026-01-08 07:16:18 +0000

    style: auto fix by rubocop

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,6 +23,6 @@ Rails.application.configure do
   end
 
   config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
-  config.content_security_policy_nonce_directives = %w(script-src)
+  config.content_security_policy_nonce_directives = %w[script-src]
   config.content_security_policy_nonce_auto = true
 end
